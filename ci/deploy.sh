6bc3915ee7228e6da5e689c401b606bb290c51aa
Hadrien Milano <hadrien.milano@gmail.com>
2020-04-13 17:23:05 +0200

    fix deployment script

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
+if [ -z "$REPO_ROOT" ]; then
+    echo "Required environment variable REPO_ROOT is not set!"
+    exit 1
+fi
+
 echo "=== Configuring credentials"
 git config --global user.name "${GH_NAME}"
 git config --global user.email "${GH_EMAIL}"
 echo "machine github.com login ${GH_NAME} password ${GH_TOKEN}" > ~/.netrc
-echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
+echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > "$REPO_ROOT/.npmrc"
 
 echo "=== Deploying npm module"
-pushd dist
+cd "$REPO_ROOT/dist"
 npm publish
-popd
 
 echo "=== Deploying docs"
-pushd docs
+cd "$REPO_ROOT/docs"
 yarn
 GIT_USER="${GH_NAME}" yarn deploy
-popd
