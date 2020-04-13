c2abaa0c74ed9e3d37720ae7ee812eaa6b7856e6
Hadrien Milano <hadrien.milano@gmail.com>
2020-04-13 15:53:30 +0200

    authenticate CD script

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+echo "=== Configuring credentials"
+git config --global user.name "${GH_NAME}"
+git config --global user.email "${GH_EMAIL}"
+echo "machine github.com login ${GH_NAME} password ${GH_TOKEN}" > ~/.netrc
+echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
+
 echo "=== Deploying npm module"
 pushd dist
 npm publish
@@ -8,5 +14,5 @@ popd
 echo "=== Deploying docs"
 pushd docs
 yarn
-GIT_USER=hmil yarn deploy
+GIT_USER="${GH_NAME}" yarn deploy
 popd
